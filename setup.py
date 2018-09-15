af0e36fbd93e22560436261bfb94612aca710a5b
Melissa Nuño <dangle@contains.io>
2018-09-15 17:11:23 -0600

    Add python37 support.

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(
     license='MIT',
     py_modules=['typingplus'],
     install_requires=[
-        'typing >= 3.5.3',
-        'typing_extensions >= 3.6.2',
+        'typing >= 3.5.3 ; python_version < "3.7.0"',
+        'typing_extensions >= 3.6.2 ; python_version < "3.7.0"',
         'six >= 1.10.0'
     ],
     setup_requires=[
